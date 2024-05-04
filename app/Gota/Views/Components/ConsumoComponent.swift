2658fc45f97cd99cdb684bf919ae323915c09db6
Brenda Sofia Sandoval <bsofiasandoval2@gmail.com>
2024-05-03 18:15:24 -0600

    Algunos arreglos de front

---
+++ app/Gota/Views/Components/ConsumoComponent.swift
@@ -0,0 +1,18 @@
+//
+//  ConsumoComponent.swift
+//  Gota
+//
+//  Created by Sofia Sandoval on 5/2/24.
+//
+
+import SwiftUI
+
+struct ConsumoComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ConsumoComponent()
+}
