8ba8e96b9136a5767627b77ad87d0ac63995d363
ivanadp-19 <69006652+ivanadp-19@users.noreply.github.com>
2024-05-04 20:53:39 -0600

    Meti un pito coñito sucio y algo de gotas totales y zonas totales

---
+++ app/Gota/Views/Components/GotasTotales.swift
@@ -0,0 +1,18 @@
+//
+//  GotasTotales.swift
+//  Gota
+//
+//  Created by felipe ivan on 04/05/24.
+//
+
+import SwiftUI
+
+struct GotasTotales: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GotasTotales()
+}
---
+++ app/Gota/Views/Components/SistemasTotalesComponent.swift
@@ -0,0 +1,18 @@
+//
+//  SistemasTotalesComponent.swift
+//  Gota
+//
+//  Created by felipe ivan on 04/05/24.
+//
+
+import SwiftUI
+
+struct SistemasTotalesComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SistemasTotalesComponent()
+}
