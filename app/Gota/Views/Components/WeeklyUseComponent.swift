80ef0cdfc14f2558f8a57821686a11ac40233281
Brenda Sofia Sandoval <bsofiasandoval2@gmail.com>
2024-05-05 20:05:15 -0600

    dashboard to form view

---
+++ app/Gota/Views/Components/WeeklyUseComponent.swift
@@ -0,0 +1,18 @@
+//
+//  WeeklyUseComponent.swift
+//  Gota
+//
+//  Created by Sofia Sandoval on 5/5/24.
+//
+
+import SwiftUI
+
+struct WeeklyUseComponent: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WeeklyUseComponent()
+}
